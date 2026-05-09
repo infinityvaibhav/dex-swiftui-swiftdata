12af458e4f6ef0f3b855019b93b796ac2bf07138
Vaibhav Upadhyay <uvaibhavb@gmail.com>
2026-05-09 08:36:16 +0530

    [feat] Designed small, medium, large pokemon widgets

--- DexWidget/DexWidget.swift
+++ DexWidget/DexWidget.swift
@@ -45,20 +45,76 @@ struct SimpleEntry: TimelineEntry {
     let sprite: Image
     
     static var placeholder: Self {
-        SimpleEntry(date: .now, name: "Bulbasaur", types: ["grass", "Poison"], sprite: Image(.mew))
+        SimpleEntry(date: .now, name: "Bulbasaur", types: ["grass", "Poison"], sprite: Image(.bulbasaur))
     }
     
     static var placeholder2: Self {
-        SimpleEntry(date: .now, name: "Mew", types: ["psychic"], sprite: Image(.bulbasaur))
+        SimpleEntry(date: .now, name: "Mew", types: ["psychic"], sprite: Image(.mew))
     }
 }
 
 struct DexWidgetEntryView : View {
+    @Environment(\.widgetFamily) var widgetSize
     var entry: Provider.Entry
 
     var body: some View {
-        VStack {
-            entry.sprite
+        switch widgetSize {
+        case .systemMedium:
+            HStack {
+                pokemonImage
+                Spacer()
+                VStack(alignment: .leading, spacing: 1) {
+                    Text(entry.name.capitalized)
+                        .font(.title)
+                    
+                    HStack {
+                        typesView
+                    }
+                }
+                .layoutPriority(1)
+                Spacer()
+            }
+        case .systemLarge:
+            ZStack {
+                pokemonImage
+                
+                VStack(alignment: .leading) {
+                    Text(entry.name.capitalized)
+                        .font(.largeTitle)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.75)
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        typesView
+                    }
+                }
+            }
+        default:
+            pokemonImage
+        }
+    }
+    
+    var pokemonImage: some View {
+        entry.sprite
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .shadow(radius: 6)
+    }
+    
+    var typesView: some View {
+        ForEach(entry.types, id: \.self) { type in
+            Text(type.capitalized)
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .foregroundStyle(.black)
+                .padding(.horizontal, 13)
+                .padding(.vertical, 5)
+                .background(Color(type.capitalized))
+                .cornerRadius(5)
+                .shadow(radius: 3)
         }
     }
 }
@@ -70,19 +126,34 @@ struct DexWidget: Widget {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             if #available(iOS 17.0, *) {
                 DexWidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
+                    .foregroundStyle(.black)
+                    .containerBackground(Color(entry.types[0].capitalized), for: .widget)
             } else {
                 DexWidgetEntryView(entry: entry)
                     .padding()
                     .background()
             }
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Pokemon")
+        .description("See a random pokemon.")
     }
 }
 
 #Preview(as: .systemSmall) {
+    DexWidget()
+} timeline: {
+    SimpleEntry.placeholder
+    SimpleEntry.placeholder2
+}
+
+#Preview(as: .systemMedium) {
+    DexWidget()
+} timeline: {
+    SimpleEntry.placeholder
+    SimpleEntry.placeholder2
+}
+
+#Preview(as: .systemLarge) {
     DexWidget()
 } timeline: {
     SimpleEntry.placeholder
