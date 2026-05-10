816d8f65437f61591b3a760d4fe947783913bc77
Vaibhav Upadhyay <uvaibhavb@gmail.com>
2026-05-10 12:13:56 +0530

    [feat] Update widget to use SwiftData

--- DexWidget/DexWidgetBundle.swift
+++ DexWidget/DexWidgetBundle.swift
@@ -1,16 +1,16 @@
-////
-////  DexWidgetBundle.swift
-////  DexWidget
-////
-////  Created by वैभव उपाध्याय on 09/05/26.
-////
 //
-//import WidgetKit
-//import SwiftUI
+//  DexWidgetBundle.swift
+//  DexWidget
 //
-//@main
-//struct DexWidgetBundle: WidgetBundle {
-//    var body: some Widget {
-//        DexWidget()
-//    }
-//}
+//  Created by वैभव उपाध्याय on 09/05/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
